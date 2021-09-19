9f7ea5fbc9dae211d1728a4f79a17211ea058a31
AndersonPGS <andersonpgs2001@hotmail.com>
2021-09-19 10:29:43 -0300

    TodayList add 8 TodayDiv jumping two by two

--- src/components/BottomBar/TodayList/TodayList.js
+++ src/components/BottomBar/TodayList/TodayList.js
@@ -2,15 +2,21 @@ import Today from "./Today/Today";
 import "./TodayList.css";
 
 const TodayList = ({ weather }) => {
+  let TodayNumber = 0;
   return (
     <div className="TodayList">
       {weather.hourly.map((data, index) => {
         if (index === 0) {
+          TodayNumber++;
           return <Today actived={true} data={data} />;
-        } else if (index >= 8) {
+        } else if (TodayNumber >= 8) {
+          // eslint-disable-next-line array-callback-return
+          return;
+        } else if (index % 2 !== 0) {
           // eslint-disable-next-line array-callback-return
           return;
         } else {
+          TodayNumber++;
           return <Today actived={false} data={data} />;
         }
       })}
