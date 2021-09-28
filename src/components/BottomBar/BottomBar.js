c474d472e35d64a47c42fee4b5b6787b470097b9
AndersonPGS <andersonpgs2001@hotmail.com>
2021-09-27 21:02:56 -0300

    render list on click

--- src/components/BottomBar/BottomBar.js
+++ src/components/BottomBar/BottomBar.js
@@ -1,26 +1,35 @@
+import { useState } from "react";
 import "./BottomBar.css";
 import TodayList from "./TodayList/TodayList";
 import TomorrowList from "./TomorrowList/TomorrowList";
 import WeekList from "./WeekList/WeekList";
 
 const BottomBar = ({ weather }) => {
+  const [currentDiv, setCurrentDiv] = useState(0);
+
   return (
     <div className="BottomBar">
       <div className="BottomBar__menu">
-        <div className="BottomBar__tab actived">
+        <div className="BottomBar__tab actived" onClick={setCurrentDiv(0)}>
           <h2>Hoje</h2>
         </div>
-        <div className="BottomBar__tab">
+        <div className="BottomBar__tab" onClick={setCurrentDiv(1)}>
           <h2>Amanhã</h2>
         </div>
-        <div className="BottomBar__tab">
+        <div className="BottomBar__tab" onClick={setCurrentDiv(2)}>
           <h2>Semana</h2>
         </div>
       </div>
 
-      {/* <TodayList weather={weather} /> */}
-      {/* <TomorrowList weather={weather} /> */}
-      <WeekList weather={weather} />
+      {() => {
+        if (currentDiv === 0) {
+          return <TodayList weather={weather} />;
+        } else if (currentDiv === 1) {
+          return <TomorrowList weather={weather} />;
+        } else {
+          return <WeekList weather={weather} />;
+        }
+      }}
     </div>
   );
 };
