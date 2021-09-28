f9a02ad329dae26b631c033bb65a6c8882d279b8
AndersonPGS <andersonpgs2001@hotmail.com>
2021-09-28 10:03:39 -0300

    Create function to change bottombar infos

--- src/components/BottomBar/BottomBar.js
+++ src/components/BottomBar/BottomBar.js
@@ -7,29 +7,37 @@ import WeekList from "./WeekList/WeekList";
 const BottomBar = ({ weather }) => {
   const [currentDiv, setCurrentDiv] = useState(0);
 
+  function onClicked(index) {
+    let divCurrent = document.querySelector("#div" + index);
+    document.querySelectorAll(".actived").forEach((div) => {
+      div.classList.remove("actived");
+    });
+
+    divCurrent.classList.add("actived");
+    setCurrentDiv(index);
+  }
+
   return (
     <div className="BottomBar">
       <div className="BottomBar__menu">
-        <div className="BottomBar__tab actived" onClick={setCurrentDiv(0)}>
+        <div
+          className="BottomBar__tab actived"
+          id="div0"
+          onClick={() => onClicked(0)}
+        >
           <h2>Hoje</h2>
         </div>
-        <div className="BottomBar__tab" onClick={setCurrentDiv(1)}>
+        <div className="BottomBar__tab" id="div1" onClick={() => onClicked(1)}>
           <h2>Amanhã</h2>
         </div>
-        <div className="BottomBar__tab" onClick={setCurrentDiv(2)}>
+        <div className="BottomBar__tab" id="div2" onClick={() => onClicked(2)}>
           <h2>Semana</h2>
         </div>
       </div>
 
-      {() => {
-        if (currentDiv === 0) {
-          return <TodayList weather={weather} />;
-        } else if (currentDiv === 1) {
-          return <TomorrowList weather={weather} />;
-        } else {
-          return <WeekList weather={weather} />;
-        }
-      }}
+      {currentDiv === 0 && <TodayList weather={weather} />}
+      {currentDiv === 1 && <TomorrowList weather={weather} />}
+      {currentDiv === 2 && <WeekList weather={weather} />}
     </div>
   );
 };
